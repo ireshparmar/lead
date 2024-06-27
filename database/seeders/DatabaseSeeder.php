8f4bd239eb603718647fc621bb115d501faea8a0
iresh parmar <iresh.parmar@indianic.com>
2024-06-27 08:57:39 +0530

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(RolesSeeder::class);
         $this->call(ExpenseCategoriesSeeder::class);
+        $this->call(VisaTypeSeeder::class);
+
     }
 }
