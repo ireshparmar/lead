b05ae489701a54f275435a65c28f58f28aca91b4
iresh parmar <iresh.parmar@indianic.com>
2024-09-29 11:08:11 +0530

    fix

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -35,6 +35,21 @@ class RolesSeeder extends Seeder
                 'created_at' => Carbon::now()
             ],
         ]);
+
+        $checkAdmin = DB::table('users')->where('id',1)->count();
+        if($checkAdmin==0){
+            DB::table('users')->insert([
+                'id' => 1,
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin@123')
+            ]);
+            DB::table('model_has_roles')->insert([
+                'role_id' => 1,
+                'model_type' => 'App\Models\User',
+                'model_id' => 1,
+            ]);
+        }
         // Re-enable foreign key checks
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
